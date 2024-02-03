b977db248f3b36896db0961647724f30d1fade58
Kay <47565280+free-ko@users.noreply.github.com>
2024-02-03 17:18:20 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 237ms, Memory: 47MB, Status: Passed - CodeTree

--- 240203/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
+++ 240203/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
@@ -3,6 +3,12 @@ const printSquare = (row, column) => {
 
     arrays.forEach((items) => {
         const test = items.reduce((acc, cur) => acc + cur)
+        
         console.log(test)
     })
 }
+
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let [rowNum, colNum] = input.map(Number);
+printSquare(rowNum, colNum);
