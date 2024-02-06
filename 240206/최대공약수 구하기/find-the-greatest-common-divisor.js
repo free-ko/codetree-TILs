b7658ef6e83067b5ca727b9536e233aad64e20a2
Kay <47565280+free-ko@users.noreply.github.com>
2024-02-06 22:00:54 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240206/최대공약수 구하기/find-the-greatest-common-divisor.js
+++ 240206/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -4,7 +4,7 @@ let [n, m] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 const printGCD = (n, m) => {
   let result = 0;
   
-  for(let i = 0; i < Math.min(n, m); i++) {
+  for(let i = 1; i <= Math.min(n, m); i++) {
     if(n % i === 0 && m % i === 0) {
       result = i;
     }
